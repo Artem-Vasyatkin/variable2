6aa19e1f9fcb9e05db6cb9cc73890f9e37c3d63a
Artem Vasyatkin <dustler98@gmail.com>
2024-03-27 13:49:43 +0300

    FF

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        float sugar = 3;
+        float onePortion = sugar / 4;
+        System.out.println("Порция сахара весит " + onePortion + " кг");
+
+        byte a = 1;
+        short b = 1;
+        int c = 1;
+        int d = a + b + c;
+        System.out.println(d);
+
+        byte e = 1;
+        float g = e + 1f;
+        System.out.println(g);
+    }
+
+}
